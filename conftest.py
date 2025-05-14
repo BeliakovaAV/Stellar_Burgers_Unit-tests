0ed5b5154304c9bafdb4f31d3f557aac3fb09aa6
Alya Beliakova <alyabelya117@gmail.com>
2025-05-14 17:29:36 +0400

    data and test_burger files added, conftest file updated

--- conftest.py
+++ conftest.py
@@ -1,16 +1,32 @@
 import pytest
-
+import data
 from bun import Bun
+from ingredient import Ingredient
+from burger import Burger
+import ingredient_types
 
 
 @pytest.fixture
 def wholegrain_bun():
-    return Bun("Цельнозерновая", 1.54)
+    return Bun(data.BUN_NAME, data.BUN_PRICE)
+
+@pytest.fixture
+def cheese_sauce():
+    return Ingredient(ingredient_types.INGREDIENT_TYPE_SAUCE, data.SAUCE_NAME, data.SAUCE_PRICE)
+
+@pytest.fixture
+def meat_filling():
+    return Ingredient(ingredient_types.INGREDIENT_TYPE_FILLING, data.FILLING_NAME, data.FILLING_PRICE)
+
+
+@pytest.fixture
+def empty_burger():
+    return Burger()
+
+@pytest.fixture
+def bun_burger(empty_burger, wholegrain_bun):
+    empty_burger.set_buns(wholegrain_bun)
+    return empty_burger
 
 
 
-# @pytest.fixture
-# def book_with_1_favorite_book(book):
-#   book.add_new_book('Сияние')
-#  book.add_book_in_favorites('Сияние')
-# return book
---
+++ tests/test_burger.py
@@ -0,0 +1,55 @@
+import pytest
+from bun import Bun
+from ingredient import Ingredient
+import data
+import ingredient_types
+
+
+class TestBurger:
+    def test_set_buns(self, empty_burger, wholegrain_bun):
+        empty_burger.set_buns(wholegrain_bun)
+        assert empty_burger.bun == wholegrain_bun
+
+    def test_add_ingredient(self, bun_burger, meat_filling):
+        bun_burger.add_ingredient(meat_filling)
+        assert bun_burger.ingredients == [meat_filling]
+
+    def test_remove_ingredient(self, bun_burger, cheese_sauce):
+        bun_burger.add_ingredient(cheese_sauce)
+        bun_burger.remove_ingredient(0)
+        assert bun_burger.ingredients == []
+
+    def test_remove_no_ingredient(self, bun_burger):
+        with pytest.raises(IndexError):
+            bun_burger.remove_ingredient(0)
+
+    def test_move_ingredient(self, bun_burger, meat_filling, cheese_sauce):
+        bun_burger.add_ingredient(meat_filling)
+        bun_burger.add_ingredient(cheese_sauce)
+        bun_burger.move_ingredient(0, 1)
+        assert bun_burger.ingredients == [cheese_sauce, meat_filling]
+
+    @pytest.mark.parametrize("bun_price, ingredient_price, expected_total", [
+        (1.05, [], 2.1),
+        (7.0, [0.8], 14.8),
+        (5.3, [1.6, 4.4], 16.6),
+    ])
+    def test_get_price_different(self, empty_burger, bun_price, ingredient_price, expected_total):
+        empty_burger.set_buns(Bun(data.BUN_NAME, bun_price))
+        for price in ingredient_price:
+            empty_burger.add_ingredient(Ingredient(ingredient_types.INGREDIENT_TYPE_FILLING, data.FILLING_NAME, price))
+        assert empty_burger.get_price() == expected_total
+
+    def test_get_receipt(self, bun_burger, meat_filling, cheese_sauce):
+        bun_burger.add_ingredient(meat_filling)
+        bun_burger.add_ingredient(cheese_sauce)
+        receipt = bun_burger.get_receipt()
+        total = bun_burger.get_price()
+        expected = "\n".join([
+            f"(==== {data.BUN_NAME} ====)",
+            f"= {ingredient_types.INGREDIENT_TYPE_FILLING.lower()} {data.FILLING_NAME} =",
+            f"= {ingredient_types.INGREDIENT_TYPE_SAUCE.lower()} {data.SAUCE_NAME} =",
+            f"(==== {data.BUN_NAME} ====)\n",
+            f"Price: {total}"
+        ])
+        assert receipt == expected
